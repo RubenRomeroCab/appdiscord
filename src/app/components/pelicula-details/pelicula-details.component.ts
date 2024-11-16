cadf1d6c2fa81697988041d97b3dde33e8d845a5
RubenRomeroCab <rubenromerodev@gmail.com>
2024-11-16 19:29:31 +0100

    Pipe Url
    iframe funcionando

--- src/app/components/pelicula-details/pelicula-details.component.ts
+++ src/app/components/pelicula-details/pelicula-details.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { ActivatedRoute } from '@angular/router';
 import { PeliculaModel } from '../../models/peliculas.model';
-import { Subscription } from 'rxjs';
+import { DomseguroPipe } from '../../pipes/domseguro.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pelicula-details',
   standalone: true,
-  imports: [],
+  imports: [DomseguroPipe,CommonModule],
   templateUrl: './pelicula-details.component.html',
   styleUrl: './pelicula-details.component.scss'
 })
 export class PeliculaDetailsComponent implements OnInit {
 
   pelicula!:PeliculaModel | null;
+  link!:string | null
 
   constructor(private peliculaService:PeliculasService,
               private activateRouter:ActivatedRoute
@@ -26,6 +28,12 @@ export class PeliculaDetailsComponent implements OnInit {
       this.peliculaService.getPeliculaById(id).subscribe((data:PeliculaModel|undefined) =>{
       if(data)
         this.pelicula= data;
+      console.log(this.pelicula)
+      if(this.pelicula){
+        this.link = this.pelicula.trailer.replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/");
+      }
+      
+     
       })
     }
     
