000a7fab6845d05eb275c434a3d44325de0f24ec
RubenRomeroCab <rubenromerodev@gmail.com>
2024-11-25 17:03:15 +0100

    Reto2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
-    firebase: {
-        apiKey: process.env['FIREBASE_API_KEY'],
-        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-        projectId: process.env['FIREBASE_PROJECT_ID'],
-        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-        appId: process.env['FIREBASE_APP_ID'],
-        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-        measurementId: process.env['FIREBASE_MEASUREMENT_ID'],
-      },
+  firebase:{
+    "projectId":"appdiscord-5a525","appId":"1:85065074041:web:19b6d81690c8192e13dba8","storageBucket":"appdiscord-5a525.firebasestorage.app","apiKey":"AIzaSyCYIjme1nn3mBIvV_TC-6AvHCYRvCZZMJA","authDomain":"appdiscord-5a525.firebaseapp.com","messagingSenderId":"85065074041","measurementId":"G-YJQMD8TNGF"
+},
 };
