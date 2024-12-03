ed4837e015c904a747b17dfa3107cc0e53a066aa
Dsks <franpe1304@gmail.com>
2024-12-03 13:38:38 +0100

    Added user to navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { User } from 'firebase/auth';
@@ -7,20 +7,31 @@ import { User } from 'firebase/auth';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  
-  constructor(private serviceUser:AuthService){
-  
+
+  user: User | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { 
+    this.authService.getCurrentUserObservable().subscribe(
+      (user) => {
+        this.user = user;
+      }
+    );
   }
 
+  login() {
+    this.router.navigate(['login']);
+  }
 
-
-salir(){
-  this.serviceUser.logout();
-}
+  logout() {
+    this.authService.logout();
+  }
 
 }
