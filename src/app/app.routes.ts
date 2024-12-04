311d40a02e85f95afbd511813e251b373c57e921
Dsks <franpe1304@gmail.com>
2024-12-04 18:18:52 +0100

    Added Basic profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,14 @@ import { HomeComponent } from './components/navegacion/home/home.component';
 import { authGuard } from './guards/auth.guard';
 import { MoviesComponent } from './components/navegacion/movies/movies.component';
 import { SeriesComponent } from './components/navegacion/series/series.component';
-import { PerfileComponent } from './components/user/perfile/perfile.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
 import { CreateMoviesComponent } from './components/navegacion/create-movies/create-movies.component';
 import { MovieDetailsComponent } from './components/navegacion/movie-details/movie-details.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'pelicula-detail/:id',component:MovieDetailsComponent},
-    {path:'perfile',component:PerfileComponent,canActivate:[authGuard]},
+    {path:'perfile',component:ProfileComponent,canActivate:[authGuard]},
     {path:'series',component:SeriesComponent},
     {path:'peliculas',component:MoviesComponent},
     {path:'create',component:CreateMoviesComponent,canActivate:[authGuard]},
---
+++ src/app/components/User/profile/profile.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AppUser } from '../../../models/appuser.model';
+import { AuthService } from '../../../services/auth.service';
+import { UsersService } from '../../../services/users.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+
+  user: AppUser | null = null;
+  profileForm!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+    private fb: FormBuilder,
+    private snackbar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    this.loadUserProfile();
+  }
+
+  private loadUserProfile(): void {
+    this.authService.getCurrentUser().subscribe((firebaseUser) => {
+      if (!firebaseUser) return;
+
+      this.usersService.getUserById(firebaseUser.uid).subscribe((appUser) => {
+        this.user = appUser || null;
+
+        // Inicializar el formulario con los datos del usuario
+        this.profileForm = this.fb.group({
+          name: [appUser?.name || '', [Validators.required, Validators.minLength(3)]],
+          avatar: [appUser?.avatar || '', [Validators.pattern(/(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/)]],
+          reputation: [{ value: appUser?.reputation || 0, disabled: true }],
+          likedGenres: [{ value: appUser?.likedGenres || {}, disabled: true }]
+        });
+      });
+    });
+  }
+
+  saveProfile(): void {
+    if (!this.profileForm.valid || !this.user) return;
+
+    const { name, avatar } = this.profileForm.value;
+
+    // Actualizar Firestore
+    this.usersService.updateUser(this.user.id, { name, avatar }).subscribe({
+      next: () => {
+        this.snackbar.open('Profile updated successfully', undefined, { duration: 3000 });
+        this.loadUserProfile(); // Recargar perfil actualizado
+      },
+      error: () => {
+        this.snackbar.open('Error updating profile in Firestore', undefined, { duration: 3000 });
+      }
+    });
+  }
+
+}
+
